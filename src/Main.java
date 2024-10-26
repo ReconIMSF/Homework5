a666ee7822bd64089f2a57960ffc222401e09545
ReconIMSF <dorbatorg@gmail.com>
2024-10-26 21:20:42 +0800

    Homework 5 - some remaining mistakes are corrected

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         System.out.println("Задача 3");
         short year = 2021;
         boolean leapYearStart = year >= 1584;
-        if (leapYearStart && year % 4 == 0 && year % 100 != 0 && year % 400 == 0) {
+        if (leapYearStart && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else if (leapYearStart) {
             System.out.println(year + " год не является високосным");
@@ -98,35 +98,21 @@ public class Main {
         byte monthNumber = 12;
         switch (monthNumber) {
             case 1:
-                System.out.println("Зима");
-                break;
             case 2:
                 System.out.println("Зима");
                 break;
             case 3:
-                System.out.println("Весна");
-                break;
             case 4:
-                System.out.println("Весна");
-                break;
             case 5:
                 System.out.println("Весна");
                 break;
             case 6:
-                System.out.println("Лето");
-                break;
             case 7:
-                System.out.println("Лето");
-                break;
             case 8:
                 System.out.println("Лето");
                 break;
             case 9:
-                System.out.println("Осень");
-                break;
             case 10:
-                System.out.println("Осень");
-                break;
             case 11:
                 System.out.println("Осень");
                 break;
